8333b544dda28fc1878121fbc155f25f3e1b2a50
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-03-06 13:39:29 +0100

    Add user sign up functionality

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve, reject) => {
+    resolve({
+      firstName,
+      lastName,
+    });
+  });
+}
