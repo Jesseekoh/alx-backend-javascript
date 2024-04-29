6417e058f50ef1591ed73be8e86be8fddcf49a2e
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-04-30 00:34:34 +0100

    task 0: fix requirement failure 3

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,8 @@
  * Displays a message to stdout
  * @param {String} str the message to log to the console
  */
-function displayMessage (str) {
+const displayMessage = (str) => {
   console.log(str);
-}
+};
 
 module.exports = displayMessage;
